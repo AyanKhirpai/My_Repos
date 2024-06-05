13a001d9f81efce6e7b0ada3efaa322a97a05ab8
AyanKhirpai <144482564+AyanKhirpai@users.noreply.github.com>
2024-06-05 14:36:37 +0530

    Update example.py

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to the Sum Calculator2!'
+    return render_template('index.html')
 
 @app.route('/sum', methods=['POST'])
 def sum_numbers():
